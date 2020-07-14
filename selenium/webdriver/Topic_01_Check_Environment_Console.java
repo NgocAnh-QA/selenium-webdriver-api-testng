054a0bc1ff638aff0ab36f178bb13393d12abb89
Ngoc Anh <ngocanhqa@gmail.com>
2020-07-14 22:50:02 +0700

    Init new repository

---
+++ selenium/webdriver/Topic_01_Check_Environment_Console.java
@@ -0,0 +1,17 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Topic_01_Check_Environment_Console {
+
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		System.out.println("Selenium Automation Testing");
+		driver = new FirefoxDriver();
+
+		driver.get("https://www.facebook.com/");
+	}
+
+}
