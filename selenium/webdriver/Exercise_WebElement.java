796cfc02c491a6fa78098b5f2a29568dcdb41fb9
Ngoc Anh <ngocanhqa@gmail.com>
2020-07-26 17:54:09 +0700

    Update TC_02_WebElement

--- selenium/webdriver/Exercise_WebElement.java
+++ selenium/webdriver/Exercise_WebElement.java
@@ -18,39 +18,96 @@ public class Exercise_WebElement {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+
 		// Step 01
 		driver.get("https://automationfc.github.io/basic-form/index.html");
 		Thread.sleep(2000);
 
 	}
-	
+
 	@Test
 	public void TC_01_CheckDisplayed() throws InterruptedException {
 		// Step 02
-		WebElement mail=  driver.findElement(By.id("mail"));
+		WebElement mail = driver.findElement(By.id("mail"));
 		WebElement education = driver.findElement(By.id("edu"));
 		WebElement age = driver.findElement(By.id("under_18"));
-		
-		if(mail.isDisplayed()) {
+
+		if (mail.isDisplayed()) {
 			mail.sendKeys("Automation Testing");
 			Thread.sleep(2000);
 		}
-		
-		if(education.isDisplayed()) {
+
+		if (education.isDisplayed()) {
 			education.sendKeys("Automation Testing");
 			Thread.sleep(2000);
 		}
-		
+
 		if (age.isDisplayed()) {
 			age.click();
 			Thread.sleep(2000);
 		}
-		
-		
+
 	}
-	
-	
+
+	@Test
+	public void TC_02_CheckEnabled() {
+		// Step 02
+		String textEnabled = "Element is enabled";
+		String textDisabled = "Element is disabled";
+		if (driver.findElement(By.id("mail")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.id("edu")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.id("under_18")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.id("job1")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.name("user_job2")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.id("development")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.name("slider-1")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.id("password")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.id("radio-disabled")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.id("bio")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.name("user_job3")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.id("check-disbaled")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+		if (driver.findElement(By.name("slider-2")).isEnabled()) {
+			System.out.println(textEnabled);
+		} else System.out.println(textDisabled);
+
+	}
+
 	@AfterMethod
 	public void afterClass() {
 		driver.quit();
