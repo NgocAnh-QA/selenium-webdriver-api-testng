50d0015887ab2b64edfb1e1cf04e9dcf76655cd0
Ngoc Anh <ngocanhqa@gmail.com>
2020-07-30 21:36:34 +0700

    Create Random (Name, NumberPhone, Mail)

--- Java/Libarary_Method/RandomForm.java
+++ Java/Libarary_Method/RandomForm.java
@@ -4,7 +4,20 @@ public class RandomForm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
+	public String RanDomName() {
+		return "";
+	}
+	
+	public String RanDomNumberPhone() {
+		return "";
+	}
+	
+	public String RanDomMail() {
+		return "";
+	}
+	
+	
 }
