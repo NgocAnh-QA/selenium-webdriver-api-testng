dc7009906c43617cf4464b5492beabfe69f35028
NgocAnh <ngocanh.huyh9904@gmail.com>
2020-09-15 22:57:15 +0700

    Done Exercise Windows/Tab

--- selenium/webdriver/Exercise_Windows_Tab.java
+++ selenium/webdriver/Exercise_Windows_Tab.java
@@ -3,14 +3,13 @@ package webdriver;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,6 +22,7 @@ public class Exercise_Windows_Tab {
 	String IdWinDow;
 	Actions action;
 	JavascriptExecutor jsExecutor;
+	Alert alert;
 
 	@BeforeClass
 	public void beforeClass() {
@@ -30,6 +30,7 @@ public class Exercise_Windows_Tab {
 		driver = new ChromeDriver();
 		action = new Actions(driver);
 		jsExecutor = (JavascriptExecutor) driver;
+		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		// driver.manage().window().maximize();
 	}
@@ -66,7 +67,6 @@ public class Exercise_Windows_Tab {
 
 	}
 
-	@Test
 	public void TC_06() {
 		driver.get("https://kyna.vn/");
 		String parentID = driver.getWindowHandle();
@@ -79,10 +79,10 @@ public class Exercise_Windows_Tab {
 			driver.findElement(By.xpath("//a[@title='Close']")).click();
 			sleepInSecond(10);
 		}
-		// Click vào FB, Zalo, Youtube, Google Play và Fanpage
+		// Click vào FB, Youtube, Google Play và Fanpage
 
-//		clickToElementByJS("//div[@class='icon-app']/a");
-//		sleepInSecond(2);
+		clickToElementByJS("//div[@class='icon-app']/a");
+		sleepInSecond(2);
 
 		clickToElementByJS("//div[@class='social']//img[@alt='facebook']");
 		sleepInSecond(2);
@@ -90,34 +90,61 @@ public class Exercise_Windows_Tab {
 		clickToElementByJS("//div[@class='social']//img[@alt='youtube']");
 		sleepInSecond(2);
 
-//		clickToElementByJS("//div[@class='social']//img[@alt='zalo']");
-//		sleepInSecond(2);
+		switchToByTitle("Kyna.vn Official – Học Online cùng chuyên gia - Ứng dụng trên Google Play");
+		Assert.assertEquals(jsExecutor.executeScript("return document.title"),
+				"Kyna.vn Official – Học Online cùng chuyên gia - Apps on Google Play");
 
-		// switchToByTitle("Kyna.vn Official – Học Online cùng chuyên gia - Ứng dụng
-		// trên Google Play");
-		switchToByTitle("Kyna.vn - Trang ch? | Facebook"); // facebook
+		switchToByTitle("Kyna.vn - Trang chủ | Facebook"); // facebook
+		Assert.assertEquals(jsExecutor.executeScript("return document.title"), "Kyna.vn - Trang chủ | Facebook");
+
 		switchToByTitle("Kyna.vn - YouTube"); // youtube
-		// switchToByTitle("Kyna.vn"); // zalo
+		Assert.assertEquals(jsExecutor.executeScript("return document.title"), "Kyna.vn - YouTube");
+
+		driver.switchTo().window(parentID);
+
+		sleepInSecond(3);
+		closeAllWindowsWitoutParent(parentID);
 
 	}
 
-	
-	
+	@Test
 	public void TC_07() {
-		driver.get("https://kyna.vn/");
+		driver.get("http://live.demoguru99.com/index.php/");
+		sleepInSecond(3);
 		String parentID = driver.getWindowHandle();
 
-		sleepInSecond(5);
+		clickToElement("//a[text()='Mobile']");
 
-		if (driver.findElement(By.cssSelector(".fancybox-outer")).isDisplayed()) {
-			sleepInSecond(5);
-			System.out.println("<================== IF  ==================>");
-			driver.findElement(By.xpath("//a[@title='Close']")).click();
-			sleepInSecond(10);
-		}
-		WebElement element = driver.findElement(By.xpath("//div[@class='social']//img[@alt='youtube']"));
-		jsExecutor.executeScript("arguments[0].click()", element);
+		// WebElement samsung = driver.findElement(By.xpath("//a[@title='Samsung
+		// Galaxy']/following-sibling::div//a[@class='link-compare']"));
+		clickToElement("//a[@title='Samsung Galaxy']/following-sibling::div//a[@class='link-compare']");
+		Assert.assertTrue(driver.findElement(By.cssSelector(".success-msg span")).isDisplayed());
+		sleepInSecond(3);
+
+		clickToElement("//a[@title='Xperia']/following-sibling::div//a[@class='link-compare']");
+		Assert.assertTrue(driver.findElement(By.cssSelector(".success-msg span")).isDisplayed());
+		sleepInSecond(3);
+
+		clickToElement("//button[@title='Compare']"); 
+	
+		switchToWindow(parentID);
+		Assert.assertEquals(jsExecutor.executeScript("return document.title"), "Products Comparison List - Magento Commerce");
+		driver.close();
+		
+		driver.switchTo().window(parentID);
+		sleepInSecond(2);
+		
+		clickToElement("//div[@class='col-right sidebar']//a[text()='Clear All']");
+		sleepInSecond(2);
+		
+		alert = driver.switchTo().alert();
+		alert.accept();
+		
+		
+		Assert.assertTrue(driver.findElement(By.cssSelector(".success-msg span")).isDisplayed());
+		
 	}
+
 	@AfterClass
 	public void afterClass() {
 	}
@@ -163,23 +190,16 @@ public class Exercise_Windows_Tab {
 
 	public void switchToByTitle(String expectedTitle) {
 		Set<String> windows = driver.getWindowHandles();
-		ArrayList<String> titles = new ArrayList<String>();
-		
+
 		for (String a : windows) {
 			driver.switchTo().window(a);
-			titles.add(driver.getTitle().trim());
-		}
 
-		for (String b : titles) {
-			System.out.println("title ===>" + b);
-		}
+			if (driver.getTitle().equals(expectedTitle)) {
 
-		for (String b : titles) {
-			if (b.equals(expectedTitle)) {
-				driver.switchTo().window(b);
-				sleepInSecond(5);
 				break;
 			}
 		}
+		sleepInSecond(4);
+
 	}
 }
